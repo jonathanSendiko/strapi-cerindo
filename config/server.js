4e0ad91281ba296094cf43f43da7879ae292ee57
jonathanSendiko <jonathan.sendiko@gmail.com>
2021-01-02 16:06:43 +0700

    Setup Env

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST"),
+  port: env.int("PORT"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '6e8c5ec4b6f63fde777c1291f09d035c'),
+      secret: env("ADMIN_JWT_SECRET", "6e8c5ec4b6f63fde777c1291f09d035c"),
     },
   },
 });
