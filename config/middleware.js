7ab61c9973fd70d8378a32eda438a058a42eb641
jonathanSendiko <jonathan.sendiko@gmail.com>
2021-01-11 19:44:39 +0700

    component fixation and middleware

---
+++ config/middleware.js
@@ -0,0 +1,15 @@
+module.exports = {
+  load: {
+    before: ["responseTime", "logger", "cors", "responses"],
+    order: [
+      "Define the middlewares' load order by putting their name in this array in the right order",
+    ],
+    after: ["parser", "router"],
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      headers: "*",
+    },
+  },
+};
