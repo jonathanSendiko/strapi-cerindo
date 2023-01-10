78674123f3836222108abc07ad0caa952767f286
Rafiul <ahmadrafiulm@gmail.com>
2023-01-10 14:32:11 +0700

    publications slug

--- config/server.js
+++ config/server.js
@@ -5,5 +5,6 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env("ADMIN_JWT_SECRET", "6e8c5ec4b6f63fde777c1291f09d035c"),
     },
+    url : "https://www.cerindocorp.com/dashboard"
   },
 });
